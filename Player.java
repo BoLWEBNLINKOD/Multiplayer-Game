d8e80ee9f623543c01362871b18f619013401ce0
BoLWEBNLINKOD <ansar.boranbai@narxoz.kz>
2025-02-22 10:13:08 +0600

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private Room currentRoom;
+    private List<Item> inventory;
+
+    public Player(Room startingRoom) {
+        this.currentRoom = startingRoom;
+        this.inventory = new ArrayList<>();
+    }
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+    public void setCurrentRoom(Room room) {
+        this.currentRoom = room;
+    }
+
+    public void addItem(Item item) {
+        inventory.add(item);
+    }
+
+    public List<Item> getInventory() {
+        return inventory;
+    }
+}
